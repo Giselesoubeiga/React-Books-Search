25adc1dd571fc92d0595bb1c2150fac19141406e
Gisele <giskey11@gmail.com>
2020-05-09 15:49:03 -0500

    body added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import HomePage from './Pages/homePage';
-import SearchPage from './Pages/searchpage';
-import SavedBooks from './Pages/savedBooks';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HomePage from "./Pages/homePage";
+import SearchPage from "./Pages/searchpage";
+import SavedBooks from "./Pages/savedBooks";
 import "./App.css";
+import Navbar from "./Components/navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <header>
-      <nav class="navbar navbar-light bg-light">
-        <span class="navbar-brand mb-0 h1">Navbar</span>
-        <Link to={`/search`}>Search</Link>
-        <Link to={`/saved`}>Saved</Link> 
-      </nav>
-      </header>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/search' component={SearchPage} />
-      <Route exact path='/saved' component={SavedBooks} />
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <div>
+    //     <navbar />
+    //   </div>
+
+    //   {/* <nav class="navbar navbar-light bg-light">
+    //     <span class="navbar-brand mb-0 h1">Navbar</span>
+    //     <Link to={`/search`}>Search</Link>
+    //     <Link to={`/saved`}>Saved</Link> 
+    //   </nav> */}
+
+    //   {/* <Route exact path="/" component={HomePage} />
+    //   <Route exact path="/search" component={SearchPage} />
+    //   <Route exact path="/saved" component={SavedBooks} /> */}
+    // </BrowserRouter>
+    <Router>
+      <div>
+        <Navbar/>
+      </div>
+      <div className="container">
+        <Route exact path="/" component={HomePage} />
+       <Route exact path="/search" component={SearchPage} />
+       <Route exact path="/saved" component={SavedBooks} />
+      </div>
+    </Router>
+    
   );
 }
 
--- client/src/Pages/homePage.js
+++ client/src/Pages/homePage.js
@@ -1,13 +1,35 @@
 import React from "react";
 
-
 function HomePage() {
- return(
-     <h1>HOOOOME</h1>
- )
+  return (
+    <div>
+      <form methode="POST">
+        <div className="searchform">
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Search"
+          />
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+
+      <div className="media searchItem">
+        <img src="https://picsum.photos/200/300" className="mr-3" alt="..." />
+        <div className="media-body">
+          <h5 className="mt-0">Media heading</h5>
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+          scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+          vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
+          vulputate fringilla. Donec lacinia congue felis in faucibus.
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
-
 
 export default HomePage;
