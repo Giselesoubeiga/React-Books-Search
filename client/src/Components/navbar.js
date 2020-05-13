ca62839cf61854e584047304d0e46dc90b410084
Gisele <giskey11@gmail.com>
2020-05-13 16:17:11 -0500

    fiexed navbar

--- client/src/Components/navbar.js
+++ client/src/Components/navbar.js
@@ -1,49 +1,48 @@
 import React from "react";
-import{Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark mb-5">
-        <Link className="navbar-brand" to="/">
-         Books Search
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarCollapse"
-          aria-controls="navbarCollapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/saved">
-                Save
-              </Link>
-            </li>
-
-          </ul>
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            Books Search
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">
+                  Home <span className="sr-only">(current)</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/saved">
+                  Saved Books
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
+
       <div className="container">
-      <div className="jumbotron">
-        <h1>Google Books Search</h1>
-        <p className="lead">
-        Search for and Save Books of Interest
-        </p>
+        <div className="jumbotron">
+          <h1>Google Books Search</h1>
+          <p className="lead">Search for and Save Books of Interest</p>
+        </div>
       </div>
-      </div>
-     
     </div>
   );
 }
